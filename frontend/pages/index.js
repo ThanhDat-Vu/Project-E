1d2d1a81176ca8f9ae114a5654f4b58c361823c0
VuThanhDat-UIT <20521181@gm.uit.edu.vn>
2022-06-15 21:43:23 +0700

    feat: connect and fetch data from Sanity

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,17 @@
+import { getClient } from '@lib/client';
+
 export default function Home() {
-  return (
-		<div></div>
-  )
+	return <div>Hello World</div>;
+}
+
+// Reference: https://nextjs.org/docs/basic-features/data-fetching/get-static-props
+export async function getStaticProps() {
+	const heroBannerQuery = '*[_type == "heroBanner"]';
+	const heroBannerData = await getClient(true).fetch(heroBannerQuery);
+	console.log(heroBannerData)
+	return {
+		props: {
+			heroBannerData,
+		},
+	};
 }
