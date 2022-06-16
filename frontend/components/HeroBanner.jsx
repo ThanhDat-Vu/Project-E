58ecae7453aee1602a8540ecae10df4f3d2d31b2
VuThanhDat-UIT <20521181@gm.uit.edu.vn>
2022-06-16 21:29:20 +0700

    feat: build a Hero Banner

---
+++ frontend/components/HeroBanner.jsx
@@ -0,0 +1,24 @@
+import { urlFor } from '@lib/sanity';
+
+export default function HeroBanner({ data }) {
+	return (
+		<div className='mx-0 sm:mx-8 text-white relative'>
+			<div className='h-[34rem] overflow-hidden relative'>
+				<img
+					src={urlFor(data.heroImage)}
+					className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-h-full object-cover'
+				/>
+			</div>
+
+			<div className='absolute inset-0 inline h-1/4 px-8 my-auto space-y-4 text-center sm:text-left'>
+				<div className='p-4 sm:p-0 bg-black/50 sm:bg-black/0'>
+					<p className='text-3xl'>{data.title}</p>
+					<p className='whitespace-pre-line leading-8'>{data.message}</p>
+				</div>
+				<button className='text-black font-medium bg-white px-6 py-3 rounded-sm hover:bg-gray-200'>
+					{data.cta}
+				</button>
+			</div>
+		</div>
+	);
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,19 @@
 import { getClient } from '@lib/client';
+import HeroBanner from 'components/HeroBanner';
 
-export default function Home() {
-	return <div>Hello World</div>;
+export default function Home({ heroBannerData }) {
+	return (
+		<div>
+			<HeroBanner data={heroBannerData[0]} />
+		</div>
+	);
 }
 
 // Reference: https://nextjs.org/docs/basic-features/data-fetching/get-static-props
 export async function getStaticProps() {
 	const heroBannerQuery = '*[_type == "heroBanner"]';
 	const heroBannerData = await getClient(true).fetch(heroBannerQuery);
-	console.log(heroBannerData)
+
 	return {
 		props: {
 			heroBannerData,
