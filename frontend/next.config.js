503ec5d7888445aa1bebc8414df06a2ba80d5ead
VuThanhDat-UIT <20521181@gm.uit.edu.vn>
2022-06-25 15:42:38 +0700

    feat: set up Admin path

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
 const nextConfig = {
+	rewrites: () => [STUDIO_REWRITE],
 	reactStrictMode: true,
 };
 
